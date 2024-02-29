2df627e9db3fb6d06d381d0345cf43a842a2ea89
rfernand <rfernand@microsoft.com>
2024-02-28 16:35:11 -0800

    %1

--- linux/config_git.sh
+++ linux/config_git.sh
@@ -1,6 +1,4 @@
 git config --global user.email "rfernand@microsoft.com"
 git config --global user.name 'rfernand'
-git config --global user.password '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'
 
-git config credential.helper store
 

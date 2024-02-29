ee3c00546c0b39ad4c9b219e05d4e0962aa4b772
Roland Fernandez <rfernand@microsoft.com>
2024-02-29 14:31:45 -0800

    %1

---
+++ linux/apply_creds_linux.py
@@ -0,0 +1,33 @@
+# apply_creds_linux.py: update various creds (linux version)
+import os
+import json
+import yaml
+
+# read creds
+fn = "~/rlf/test_data.yaml"
+with open(fn, "rt", encoding="utf-8") as f:
+    data = yaml.safe_load(f)
+
+# write permanent env vars
+cmd_text = "written by apply_creds_linux.py\n\n"
+for name in ["wandb_api_key", "tpx_sql_pw", "openai_api_key"]:
+    value = data[name]["value"]
+    cmd = f"export {name.upper()}={value}"
+    cmd_text += cmd + "\n"
+
+# append this to .bashrc
+fn = "~/.bashrc"
+with open(fn, "a") as f:
+    f.write(cmd_text)
+
+# write azure store files
+for store in data["azure_stores"]:
+    fn = "~/.xt/stores/{}/keys.bin".format(store)
+    # create dir, if needed
+    os.makedirs(os.path.dirname(fn), exist_ok=True)
+
+    with open(fn, "wt") as f:
+        store_dict = data["azure_stores"][store]
+        jtext = json.dumps(store_dict, indent=4)
+        f.write(jtext)
+
---
+++ windows/apply_creds_win.py
@@ -0,0 +1,27 @@
+# apply_creds_win.py: update various creds (windows version)
+import os
+import json
+import yaml
+
+# read creds
+fn = "c:/rlf/test_data.yaml"
+with open(fn, "rt", encoding="utf-8") as f:
+    data = yaml.safe_load(f)
+
+# write permanent env vars
+for name in ["wandb_api_key", "tpx_sql_pw", "openai_api_key"]:
+    value = data[name]["value"]
+    cmd = f"setx {name.upper()} {value}"
+    os.system(cmd)
+
+# write azure store files
+for store in data["azure_stores"]:
+    fn = "{}/.xt/stores/{}/keys.bin".format(os.getenv("USERPROFILE"), store)
+    # create dir, if needed
+    os.makedirs(os.path.dirname(fn), exist_ok=True)
+
+    with open(fn, "wt") as f:
+        store_dict = data["azure_stores"][store]
+        jtext = json.dumps(store_dict, indent=4)
+        f.write(jtext)
+
